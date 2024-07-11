d23fa53789c260cd737b4612b44ca010f32c72f9
Aluno ProWay <aluno@proway.treina>
2024-07-11 18:09:59 -0300

    Adicionado consulta de contatos

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 const express = require('express')
 const app = express()
+const pg = require('pg')
+
+const pool =  new pg.Pool(
+  {connectionString: process.env.DATABASE_URL})
+
+/*const pool = new pg.Pool({
+  host: process.env.PGHOST,
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  database: process.env.PGDATABASE,
+  port: process.env.PGPORT
+})*/
 
 app.get('/ola',(req, res)=>{
   res.status(200).send('Ola')
@@ -9,6 +21,17 @@ app.get('/hello',(req, res)=>{
   res.status(200).send('hello')
 })
 
-app.listen(process.env.PORT, () => {
-  console.log(`Example app listening on port ${process.env.PORT}`)
+app.get('/contatos',async (req, res) => {
+  try {
+    let cliente = await pool.connect()
+      let dados = await cliente.query('select * from tb_contatos')
+      cliente.end()
+      res.send(dados.rows)
+  } catch (error) {
+    res.status(400).send(error.message)
+  }
+})
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Example app listening on port ${process.env.PORT || 3000}`)
 })
