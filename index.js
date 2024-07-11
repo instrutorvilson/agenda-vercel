4494dbcab07a08c8e92146e9257edb1d60091148
ValdemirCandido <valdemircandido733@gmail.com>
2024-07-10 21:24:03 -0300

    desfazendo

--- index.js
+++ index.js
@@ -1,17 +1,8 @@
-require('dotenv').config()
 const express = require('express')
-const cors = require('cors')
 
-const contatos = require('./contatos')
-const produtos = require('./produtos')
-//const { verificaToken } = require('./midlewares')
-
-const app = express()
-app.use(express.json())
-app.use(cors())
-
-app.use('/contatos', contatos)
-app.use('/produtos', produtos)
+app.get('/ola',(req, res)=>{
+  res.status(200).send('Ola')
+})
 
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${process.env.PORT}`)
