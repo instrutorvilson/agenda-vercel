5801c358b266a59e75b7906cbceaf431db5e38aa
ValdemirCandido <valdemircandido733@gmail.com>
2024-07-11 17:34:11 -0300

    adicionado endpoint hello

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ app.get('/ola',(req, res)=>{
   res.status(200).send('Ola')
 })
 
+app.get('/hello',(req, res)=>{
+  res.status(200).send('hello')
+})
+
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${process.env.PORT}`)
 })
